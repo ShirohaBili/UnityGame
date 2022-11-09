97a09c7e385091d62d883d9c2dc05d18a3de87f0
白羽 <107621920+ShirohaBili@users.noreply.github.com>
2022-11-09 23:26:06 +0800

    Add files via upload

---
+++ Assets/Script/ActionController/FlyActionManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyActionManager : SSActionManager {
+    public DiskFlyAction fly;  
+    public Controller scene_controller;           
+
+    protected void Start() {
+        scene_controller = (Controller)SSDirector.GetInstance().CurrentScenceController;
+        scene_controller.action_manager = this;     
+    }
+
+    //飞碟飞行
+    public void DiskFly(GameObject disk, float angle, float power) {
+        int lor = 1;
+        if (disk.transform.position.x > 0) lor = -1;
+        fly = DiskFlyAction.GetSSAction(lor, angle, power);
+        this.RunAction(disk, fly, this);
+    }
+}
